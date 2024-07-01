61ae6b41f93a7f891454782ba7a29e36575a6d75
Mishal Cheruveettil <c.mishal46@gmail.com>
2024-07-01 12:25:13 +0400

    asdads

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,10 +7,10 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Adrian</span>
+        <span className='font-semibold mx-2 text-white'>Mishal</span>
         👋
         <br />
-        A Software Engineer from Croatia 🇭🇷
+        A Software Developer from Dubai UAE
       </h1>
     );
 
@@ -47,15 +47,15 @@ const HomeInfo = ({ currentStage }) => {
   if (currentStage === 4) {
     return (
       <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
-      </p>
+        <p className='font-medium sm:text-xl text-center'>
+          Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
+        </p>
 
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        Let's talk
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
+        <Link to='/contact' className='neo-brutalism-white neo-btn'>
+          Let's talk
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
     );
   }
 
